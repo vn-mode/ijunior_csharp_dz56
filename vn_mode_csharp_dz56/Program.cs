9f9d619b71dea2f891c6e0679976eb592e2d30a1
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-26 18:04:48 +0300

    ijunior_csharp_dz56

--- vn_mode_csharp_dz56/Program.cs
+++ vn_mode_csharp_dz56/Program.cs
@@ -7,7 +7,6 @@ public class Program
     public static void Main()
     {
         var soldiers = Soldier.CreateSampleSoldiers();
-
         var soldierService = new SoldierService();
         soldierService.DisplaySoldierNamesAndRanks(soldiers);
     }
@@ -15,7 +14,7 @@ public class Program
 
 public class SoldierService
 {
-    const string OutputFormat = "{0} - {1}";
+    private const string OutputFormat = "{0} - {1}";
 
     public void DisplaySoldierNamesAndRanks(IEnumerable<Soldier> soldiers)
     {
@@ -31,37 +30,17 @@ public class SoldierService
 
 public class Soldier
 {
-    private string name;
-    private string weapon;
-    private string rank;
-    private int serviceDuration;
+    public string Name { get; private set; }
+    public string Weapon { get; private set; }
+    public string Rank { get; private set; }
+    public int ServiceDuration { get; private set; }
 
     public Soldier(string name, string weapon, string rank, int serviceDuration)
     {
-        this.name = name;
-        this.weapon = weapon;
-        this.rank = rank;
-        this.serviceDuration = serviceDuration;
-    }
-
-    public string Name
-    {
-        get { return name; }
-    }
-
-    public string Weapon
-    {
-        get { return weapon; }
-    }
-
-    public string Rank
-    {
-        get { return rank; }
-    }
-
-    public int ServiceDuration
-    {
-        get { return serviceDuration; }
+        Name = name;
+        Weapon = weapon;
+        Rank = rank;
+        ServiceDuration = serviceDuration;
     }
 
     public static List<Soldier> CreateSampleSoldiers()
