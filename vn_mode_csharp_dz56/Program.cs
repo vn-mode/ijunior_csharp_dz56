4e605bc87b200eeafc1d0d06c415c51f9a95c270
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2024-02-23 17:49:53 +0300

    ijunior_csharp_dz56

--- vn_mode_csharp_dz56/Program.cs
+++ vn_mode_csharp_dz56/Program.cs
@@ -6,31 +6,26 @@ public class Program
 {
     public static void Main()
     {
-        SoldierFactory soldierFactory = new SoldierFactory();
-        IEnumerable<Soldier> soldiers = soldierFactory.CreateSampleSoldiers();
-        SoldierService soldierService = new SoldierService();
-
-        var simpleSoldiers = soldierService.GetSimpleSoldiers(soldiers);
-
-        foreach (var simpleSoldier in simpleSoldiers)
-        {
-            Console.WriteLine($"{simpleSoldier.Name} - {simpleSoldier.Rank}");
-        }
-    }
-}
-
-public class SoldierFactory
-{
-    public List<Soldier> CreateSampleSoldiers()
-    {
-        return new List<Soldier>
+        List<Soldier> soldiers = new List<Soldier>
         {
             new Soldier("Алексей", "Винтовка", "Рядовой", 12),
             new Soldier("Иван", "Пистолет", "Сержант", 24),
             new Soldier("Петр", "Снайперская винтовка", "Капитан", 36),
             new Soldier("Николай", "Винтовка", "Лейтенант", 48),
             new Soldier("Томас", "Винтовка", "Майор", 60)
+
         };
+
+        var newSoldiers = soldiers.Select(soldier => new
+        {
+            Info = soldier.Name + " - " + soldier.Rank
+        }
+        ).ToList();
+
+        foreach (var soldier in newSoldiers)
+        {
+            Console.WriteLine(soldier.Info);
+        }
     }
 }
 
@@ -48,12 +43,4 @@ public class Soldier
     public string Weapon { get; }
     public string Rank { get; }
     public int ServiceDuration { get; }
-}
-
-public class SoldierService
-{
-    public IEnumerable<(string Name, string Rank)> GetSimpleSoldiers(IEnumerable<Soldier> soldiers)
-    {
-        return soldiers.Select(soldier => (soldier.Name, soldier.Rank));
-    }
 }
